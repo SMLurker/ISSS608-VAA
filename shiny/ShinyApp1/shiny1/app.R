8549d4f1e177acac4ef6cce073661cbd39870d73
SMLurker <t0905685@gmail.com>
2023-06-24 15:03:18 +0800

    update

--- shiny/ShinyApp1/shiny1/app.R
+++ shiny/ShinyApp1/shiny1/app.R
@@ -1,44 +1,80 @@
 
 
-pacman::p_load(shiny,shinythemes,tools, jsonlite,tidygraph, ggraph,visNetwork)
+#### Importing R pacakges
 
-# Define UI for application that draws a histogram
+pacman::p_load(jsonlite,tidygraph,ggraph,visNetwork,graphlayouts,ggforce,skimr, 
+               tidytext,tidyverse,igraph,ggstatsplot,ggiraph,cluster,plotly,DT)
+
+# Define UI
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
+  navbarPage(
+    "Graph Visualization",
+    tabPanel(
+      "Tab 1",
+      tabsetPanel(
+        tabPanel("Sub Tab 1.1"),
+        tabPanel("Sub Tab 1.2")
+      )
+    ),
+    
+    tabPanel(
+      "Anormal Business Groups",
+      sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+          checkboxInput(inputId = "shownodesData",
+                        label = "Show nodes datatable",
+                        value = FALSE),
+          checkboxInput(inputId = "showedgesData",
+                        label = "Show edges datatable",
+                        value = FALSE)
         ),
-
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+          plotOutput("Graph"), 
+          DT::dataTableOutput(outputId = "aTable"),
+          DT::dataTableOutput(outputId = "bTable")
         )
+      )
+    ),
+    
+    tabPanel(
+      "Tab 2",
+      fluidRow(
+        column(width = 6, plotOutput("plot2"))
+      )
     )
+  )
 )
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+  
+  # Define server logic
+  server <- function(input, output) {
+    #########################################################################
+    #########################################################################
+    output$Graph <- renderPlot({
+      
+      df_graph_3 <- read_rds("data/df_graph_3.rds")
+      g_3 <- df_graph_3 %>%
+        mutate(betweenness_centrality = centrality_betweenness()) %>%
+        ggraph(layout = "kk") + 
+        geom_edge_link(aes(width = weights), alpha = 0.2) +
+        scale_edge_width(range = c(0.01, 0.1)) +
+        geom_node_point(aes(colour = group, size = betweenness_centrality)) +
+        theme_graph()
+      
+      plot(g_3)
     })
-}
+    output$aTable <- DT::renderDataTable({
+      df_nodes_3 <- read_rds("data/df_nodes_3.rds")
+      if(input$shownodesData){
+        DT::datatable(df_nodes_3)
+      }
+    }) 
+    output$bTable <- DT::renderDataTable({
+      df_edges_2 <- read_rds("data/df_edges_2.rds")
+      if(input$showedgesData){
+        DT::datatable(df_edges_2)
+      }
+    }) 
+  }
 
-# Run the application 
+# Run the app
 shinyApp(ui = ui, server = server)
